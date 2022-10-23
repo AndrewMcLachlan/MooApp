b6b13c28f727ed67e8b677e864a77b4cc762693c
Andrew McLachlan <andy@mclachlan.family>
2022-10-23 12:50:51 +1000

    Small size support. User menu. Example app.

--- src/providers/LayoutProvider.tsx
+++ src/providers/LayoutProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect } from "react";
 import { useContext } from "react";
 import * as Models from "../models";
 
@@ -6,9 +6,20 @@ export const LayoutContext = createContext<Models.LayoutOptions>({ defaultTheme:
 
 export const LayoutProvider: React.FC<React.PropsWithChildren<LayoutProviderProps>> = ({ theme, setTheme, children }) => {
 
+    const colour = document.getElementsByName("theme-color")[0];
+
+    useEffect(() => {
+        colour.setAttribute("content", theme === "dark" ? "#620000" : "#C20000");
+    }, [theme]);
+
+    const defaultTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+
+    useEffect(() => {
+        colour.setAttribute("content", defaultTheme === "dark" ? "#620000" : "#C20000");
+    }, []);
 
     return (
-        <LayoutContext.Provider value={{ theme, setTheme, defaultTheme: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light" }}>
+        <LayoutContext.Provider value={{ theme, setTheme, defaultTheme: defaultTheme}}>
             {children}
         </LayoutContext.Provider>
     );
@@ -19,4 +30,4 @@ export const useLayout = () => useContext(LayoutContext);
 export interface LayoutProviderProps extends Omit<Models.LayoutOptions, "defaultTheme"> {
 }
 
-LayoutProvider.displayName = "LayoutPRovider";
+LayoutProvider.displayName = "LayoutProvider";
