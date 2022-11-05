f4e03e4c9d5f50afbd2bb66d1bc00e42c7fd0c07
Andrew McLachlan <andy@mclachlan.family>
2022-11-05 22:04:55 +1000

    Safety checks in user menu.

--- src/layout/UserMenu.tsx
+++ src/layout/UserMenu.tsx
@@ -1,15 +1,18 @@
 import { usePhoto } from "../services/msgraph";
-import { useMsal } from "@azure/msal-react";
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
 import { OverlayTrigger, Popover } from "react-bootstrap";
 import { Avatar } from "../components";
 import { ThemeSwitcher } from "../components/ThemeSwitcher";
 
 export const UserMenu = () => {
     const msal = useMsal();
+    const isAuthenticated = useIsAuthenticated();
+
+    if (!isAuthenticated) return null;
 
     const popover = (
         <Popover id="user-popover" className="user-popover">
-            <Popover.Header as="h3"><Avatar />{msal.accounts[0].name}</Popover.Header>
+            <Popover.Header as="h3"><Avatar />{msal.accounts[0]?.name}</Popover.Header>
             <Popover.Body>
                 <ul>
                     <li><span>Theme</span><ThemeSwitcher /></li>
