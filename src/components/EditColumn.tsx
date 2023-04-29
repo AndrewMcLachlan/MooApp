31e09ff492bd62d994abe8fafc3a4476b3836d6a
Andrew McLachlan <andy@mclachlan.family>
2023-04-29 22:13:53 +1000

    Added Input and EditColumn components.

---
+++ src/components/EditColumn.tsx
@@ -0,0 +1,49 @@
+import { useClickAway } from "hooks";
+import { ValueProps } from "models";
+import { ChangeEventHandler, FocusEventHandler, KeyboardEventHandler, useEffect, useRef, useState } from "react";
+
+export const EditColumn: React.FC<EditColumnProps> = (props) => {
+
+    const ref = useRef<HTMLInputElement>();
+
+    const [editing, setEditing] = useState(false);
+    
+    useClickAway(setEditing, ref, () => {
+        props.onChange && props.onChange(ref.current?.value ?? "");
+    });
+
+    const [value, setValue] = useState(props.value);
+
+    const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setValue(e.currentTarget.value);
+    }
+
+    useEffect(() => {
+        setValue(value);
+    }, [props.value]);
+
+    const onBlur: FocusEventHandler<HTMLInputElement> = (e) => {
+        setEditing(false);
+        props.onChange && props.onChange(e.currentTarget.value);
+    }
+
+    const onKey: KeyboardEventHandler<HTMLInputElement> = (e) => {
+        if (e.key === "Enter" || e.key === "Tab") {
+            setEditing(false);
+            props.onChange && props.onChange(e.currentTarget.value);
+        }
+    }
+
+    return (
+        <td onClick={() => setEditing(true)}>
+            {!editing && props.value}
+            {editing &&
+                <input className="form-control" value={value} onChange={onChange} onBlur={onBlur} onKeyUp={onKey} ref={ref} autoFocus />
+            }
+        </td>
+    );
+}
+
+export interface EditColumnProps extends ValueProps {
+
+}
---
+++ src/models/ValueProps.ts
@@ -0,0 +1,4 @@
+export interface ValueProps {
+    value?: string;
+    onChange?: (value: string) => void;
+}
