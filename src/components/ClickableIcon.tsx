5517af288455091f3e449401a32e542839d74448
Andrew McLachlan <andy@mclachlan.family>
2023-04-29 22:03:28 +1000

    Clickabel typo.

--- src/components/ClickableIcon.tsx
+++ src/components/ClickableIcon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 
-export const ClickableIcon: React.FC<ClickableIconProps> = (props) => <FontAwesomeIcon {...props} className={classNames(props.className, "clickabel")} />;
+export const ClickableIcon: React.FC<ClickableIconProps> = (props) => <FontAwesomeIcon {...props} className={classNames(props.className, "clickable")} />;
 
 ClickableIcon.displayName = "ClickableIcon";
 
