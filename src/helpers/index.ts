92d26f6a277ffe28b2694b258946742f9d4adedd
Andrew McLachlan <andy@mclachlan.family>
2023-05-14 10:35:57 +1000

    Added missing exports.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,7 @@
 export * from "./dateHelpers";
 export * from "./guid";
 export * from "./md5";
+export * from "./paging";
 export * from "./renderingHelpers";
+export * from "./sorting";
 export * from "./trimEnd";
