eb02bc00da23398ef93f57bcdc5d0713763bffb8
Andrew McLachlan <andy@mclachlan.family>
2023-12-18 05:20:54 +1000

    Added variant support to icon link button.

--- src/components/IconLinkButton.tsx
+++ src/components/IconLinkButton.tsx
@@ -3,8 +3,8 @@ import { PropsWithChildren } from "react";
 import { ButtonProps } from "react-bootstrap";
 import { Link, LinkProps } from "react-router-dom";
 
-export const IconLinkButton: React.FC<PropsWithChildren<IconLinkButtonProps>> = ({ children, icon, ...link }) => (
-    <Link {...link}><FontAwesomeIcon icon={icon} size="xs" />{children}</Link>
+export const IconLinkButton: React.FC<PropsWithChildren<IconLinkButtonProps>> = ({ children, icon, variant, ...link }) => (
+    <Link {...link} className={`btn ${variant && `btn-${variant}`} `}><FontAwesomeIcon icon={icon} size="xs" />{children}</Link>
 );
 
 export interface IconLinkButtonProps extends Pick<FontAwesomeIconProps, "icon">, LinkProps, Pick<ButtonProps, "variant"> {
