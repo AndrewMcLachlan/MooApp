b1e2b00fb5ca449d9d32005eec6ff2494ce53b62
Andrew McLachlan <andy@mclachlan.family>
2023-12-24 13:24:01 +1000

    npm update. Added more files to npmigonre.

--- src/hooks/idParams.ts
+++ src/hooks/idParams.ts
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 
-export const useIdParams= () => {
-    const { id } = useParams<{id: string}>();
+export const useIdParams = () => {
+    const { id } = useParams<{ id: string }>();
     if (!id) throw Error("bad params");
 
     return id;
