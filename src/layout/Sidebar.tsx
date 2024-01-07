7b2eb6129d1c8e4b9a69fdf18f3a6d84f7dc30d2
Andrew McLachlan <andy@mclachlan.family>
2024-01-07 15:06:44 +1000

    Improved sidebar accessibility.

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,13 +1,12 @@
 import Chevron from "../assets/chevron.svg";
 
-import React from "react";
-import { Button, Nav } from "react-bootstrap";
-import { Link, NavLink, NavLinkProps } from "react-router-dom";
-import { NavItem } from "../models";
-import { useLayout } from "providers";
-import { isValidElement, ReactNode, useState } from "react";
 import classNames from "classnames";
 import { useLocalStorage } from "hooks";
+import { useLayout } from "providers";
+import React, { ReactNode, isValidElement } from "react";
+import { Button, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { NavItem } from "../models";
 
 export type SidebarComponent = React.FC<React.PropsWithChildren<SidebarProps>>;
 
@@ -22,7 +21,7 @@ export const Sidebar: SidebarComponent = ({ children, ...props }) => {
     };
 
     return (
-        <div className={classNames("sidebar", collapsed ? "collapsed" :"")}>
+        <div className={classNames("sidebar", collapsed ? "collapsed" : "")}>
             <Nav className="flex-column">
                 {renderMenu(props.navItems)}
                 {layout.secondaryNav.length > 0 &&
@@ -39,23 +38,23 @@ export const Sidebar: SidebarComponent = ({ children, ...props }) => {
     );
 };
 
-const renderMenu = (navItems: (NavItem|ReactNode)[]) => {
+const renderMenu = (navItems: (NavItem | ReactNode)[]) => {
 
     const items: React.ReactNode[] = navItems.map((item, index) => {
 
         if (isValidElement(item)) {
             return <React.Fragment key={`node${index}`}>{item}</React.Fragment>;
-        }; 
+        };
 
         const navItem = item as NavItem;
 
         const image = typeof navItem.image === "string" ? <img src={navItem.image} alt="" /> : navItem.image ?? <svg></svg>;
 
         if (navItem.route) {
-            return <NavLink className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`} to={navItem.route} key={`route${index}`} onClick={navItem.onClick}>{image}<span>{navItem.text}</span></NavLink>;
+            return <NavLink className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`} to={navItem.route} key={`route${index}`} onClick={navItem.onClick} title={navItem.text}>{image}<span>{navItem.text}</span></NavLink>;
         }
         else if (navItem.onClick) {
-            return <Nav.Link as={Button} key={`click${index}`} variant="link" onClick={navItem.onClick}>{image}<span>{navItem.text}</span></Nav.Link>;
+            return <Nav.Link as={Button} key={`click${index}`} variant="link" onClick={navItem.onClick} title={navItem.text}>{image}<span>{navItem.text}</span></Nav.Link>;
         }
         else {
             throw "Invalid nav item, specify a route and/or an onClick handler.";
