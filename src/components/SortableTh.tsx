87035dea4d3282dfc78f49123fa9ca71209ce94d
Andrew McLachlan <andy@mclachlan.family>
2024-03-03 22:12:59 +1000

    Fixed up SortablTh.

--- src/components/SortableTh.tsx
+++ src/components/SortableTh.tsx
@@ -1,11 +1,17 @@
 import { SortDirection } from "../models";
 import React, { HTMLAttributes, PropsWithChildren } from "react";
 import { SortIcon } from "./SortIcon";
+import classNames from "classnames";
 
-export const SortableTh: React.FC<PropsWithChildren<SortableThProps>> = ({field, sortField, sortDirection, onSort, children}) => {
+export const SortableTh: React.FC<PropsWithChildren<SortableThProps>> = ({field, sortField, sortDirection, onSort, className, children, onClick, ...rest}) => {
+
+    const clickHandler = (e: React.MouseEvent<HTMLTableCellElement>, field: string) => {
+        onSort(field);
+        onClick?.(e);
+    }
 
     return (
-        <th className="sortable" onClick={() => onSort(field)}>
+        <th className={classNames("sortable", className)} onClick={(e) => clickHandler(e, field)} {...rest}>
             {children}
             <SortIcon direction={sortDirection} hidden={field !== sortField} />
         </th>
