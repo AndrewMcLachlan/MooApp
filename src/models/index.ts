0dcf4dd1024f1a8614fc42cb3f35038c5bb55f99
Andrew McLachlan <andy@mclachlan.family>
2024-03-10 20:34:57 +1000

    Export route definition.

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,6 +4,7 @@ export * from "./Message";
 export * from "./NavItem";
 export * from "./PagedResult";
 export * from "./RagStatus";
+export * from "./Route";
 export * from "./SortDirection";
 export * from "./Theme";
 export * from "./ValueProps";
