cf97a72adb76a644c029fa4ecb86e09200b9b0ce
Andrew McLachlan <andy@mclachlan.family>
2025-03-29 21:41:00 +1000

    Ability to hide form groups.

--- moo-app/src/components/form/Group.tsx
+++ moo-app/src/components/form/Group.tsx
@@ -2,14 +2,14 @@ import React, { DetailedHTMLProps } from "react";
 import { FromGroupProvider } from "./FormGroupProvider";
 import classNames from "classnames";
 
-export type GroupComponent = React.FC<{ groupId: string } & DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>>;
+export type GroupComponent = React.FC<{ groupId: string, show?: boolean } & DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>>;
 
-export const Group: GroupComponent = ({ children, groupId, className, ...props }) => (
-    <FromGroupProvider groupId={groupId}>
+export const Group: GroupComponent = ({ children, groupId, show = true, className, ...props }) => (
+    show ? <FromGroupProvider groupId={groupId}>
         <div {...props} className={classNames(className, "form-group")}>
             {children}
         </div>
-    </FromGroupProvider>
+    </FromGroupProvider> : null
 );
 
 Group.displayName = "Group";
