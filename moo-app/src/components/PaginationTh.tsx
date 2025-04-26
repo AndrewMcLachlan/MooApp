b2297ff26f137695951696e38385ff60da7b3f53
Andrew McLachlan <andy@mclachlan.family>
2025-04-26 17:32:03 +1000

    Properly handle classname on pagination th.

--- moo-app/src/components/PaginationTh.tsx
+++ moo-app/src/components/PaginationTh.tsx
@@ -1,13 +1,14 @@
 import { PropsWithChildren } from "react";
 import { PaginationProps } from "./Paginaton";
 import { MiniPagination } from "./MiniPagination";
+import classNames from "classnames";
 
 export const PaginationTh: React.FC<PropsWithChildren<PaginationThProps>> = ({ children, ...props }) => {
 
-    const { pageNumber, numberOfPages, onChange, ...thProps } = props;
+    const { pageNumber, numberOfPages, onChange, className, ...thProps } = props;
 
     return (
-        <th {...thProps} className="pagination-th">
+        <th {...thProps} className={classNames("pagination-th", className)}>
             <div>{children}</div>
             <MiniPagination
                 pageNumber={pageNumber}
