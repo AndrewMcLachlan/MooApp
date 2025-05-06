4d97790d00b63ab2b691f034ef0ee81196484085
Andrew McLachlan <andy@mclachlan.family>
2025-05-07 07:48:36 +1000

    Added form layouts.

--- moo-app/src/components/form/Form.tsx
+++ moo-app/src/components/form/Form.tsx
@@ -1,12 +1,14 @@
+import classNames from "classnames";
 import React, { PropsWithChildren } from "react";
-import { FormProvider, SubmitHandler, useForm, UseFormReturn } from "react-hook-form";
+import { FormProvider, SubmitHandler, UseFormReturn } from "react-hook-form";
+
 import { Check, CheckComponent } from "./Check";
 import { Group, GroupComponent } from "./Group";
 import { Input, InputComponent } from "./Input";
 import { Label, LabelComponent } from "./Label";
+import { Password, PasswordComponent } from "./Password";
 import { Select, SelectComponent } from "./Select";
 import { TextArea, TextAreaComponent } from "./TextArea";
-import { Password, PasswordComponent } from "./Password";
 
 export type FormComponent<TFormValues> = React.FC<PropsWithChildren<FormProps<TFormValues>>> & {
     Check: CheckComponent;
@@ -18,11 +20,11 @@ export type FormComponent<TFormValues> = React.FC<PropsWithChildren<FormProps<TF
     TextArea: TextAreaComponent;
 };
 
-export const Form: FormComponent<any> = <TFormValues,>({ onSubmit, children, className, form, ...rest }: PropsWithChildren<FormProps<TFormValues>>) => {
+export const Form: FormComponent<any> = <TFormValues,>({ onSubmit, children, className, form, layout = "vertical", ...rest }: PropsWithChildren<FormProps<TFormValues>>) => {
 
     return (
         <FormProvider {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className={className ?? "form-container"} {...rest}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className={classNames(className ?? "form-container", `form-${layout}` )} {...rest}>
                 {children}
             </form>
         </FormProvider>
@@ -42,4 +44,5 @@ Form.displayName = "Form";
 export interface FormProps<TFormValues> extends Omit<React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement>, "onSubmit"> {
     onSubmit: SubmitHandler<TFormValues>;
     form: UseFormReturn<TFormValues>;
+    layout?: "horizontal" | "vertical";
 }
